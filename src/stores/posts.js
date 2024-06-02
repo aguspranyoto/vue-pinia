03c9032a9554e72d95989611978f2eebfa8b63e8
aguspranyoto <agusprnyt@gmail.com>
2024-06-03 06:56:00 +0700

    update

--- src/stores/posts.js
+++ src/stores/posts.js
@@ -4,64 +4,8 @@ export const usePostsStore = defineStore('posts-store', {
   // Data
   state() {
     return {
-      posts: [
-        {
-          id: 1,
-          title: 'Pengembangan Teknologi Terbaru',
-          body: 'Artikel ini membahas tentang inovasi teknologi terbaru yang sedang dikembangkan di berbagai sektor industri.',
-          author: 'Agus Pranyoto',
-          created_at: '11/06/2024',
-          is_saved: false
-        },
-        {
-          id: 2,
-          title: 'Pentingnya Pendidikan di Era Digital',
-          body: 'Era digital membawa perubahan signifikan dalam dunia pendidikan, menekankan pentingnya literasi digital bagi generasi muda.',
-          author: 'Budi Santoso',
-          created_at: '12/06/2024',
-          is_saved: false
-        },
-        {
-          id: 3,
-          title: 'Tren Kuliner 2024',
-          body: 'Tren kuliner 2024 menunjukkan peningkatan minat pada makanan sehat dan ramah lingkungan, dengan fokus pada bahan organik.',
-          author: 'Citra Dewi',
-          created_at: '13/06/2024',
-          is_saved: true
-        },
-        {
-          id: 4,
-          title: 'Transformasi Energi Terbarukan',
-          body: 'Artikel ini mengeksplorasi perkembangan energi terbarukan dan dampaknya terhadap lingkungan serta ekonomi global.',
-          author: 'Dedi Wahyudi',
-          created_at: '14/06/2024',
-          is_saved: false
-        },
-        {
-          id: 5,
-          title: 'Kesehatan Mental di Tempat Kerja',
-          body: 'Diskusi tentang pentingnya menjaga kesehatan mental di tempat kerja dan strategi untuk menciptakan lingkungan kerja yang suportif.',
-          author: 'Erika Purnama',
-          created_at: '15/06/2024',
-          is_saved: true
-        },
-        {
-          id: 6,
-          title: 'Inovasi dalam Industri Otomotif',
-          body: 'Laporan tentang berbagai inovasi terbaru dalam industri otomotif, termasuk mobil listrik dan teknologi otonom.',
-          author: 'Fajar Nugroho',
-          created_at: '16/06/2024',
-          is_saved: false
-        },
-        {
-          id: 7,
-          title: 'Perkembangan Pariwisata Pasca Pandemi',
-          body: 'Analisis tentang bagaimana industri pariwisata bangkit kembali setelah pandemi, dengan fokus pada destinasi populer dan strategi pemasaran.',
-          author: 'Gita Lestari',
-          created_at: '17/06/2024',
-          is_saved: true
-        }
-      ]
+      posts: [],
+      loading: true
     }
   },
   // computed
@@ -75,7 +19,16 @@ export const usePostsStore = defineStore('posts-store', {
         .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
   },
   // Methods
+
   actions: {
+    getPosts() {
+      fetch('http://localhost:8080/posts')
+        .then((res) => res.json())
+        .then((data) => {
+          this.posts = data
+          this.loading = false
+        })
+    },
     addPost(post) {
       this.posts.push({
         id: this.posts.length + 1,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,42 @@
 import PostItem from '@/components/PostItem.vue'
 import MyWrapper from '@/components/MyWrapper.vue'
 import { usePostsStore } from '@/stores/posts'
+import { ref } from 'vue'
 
 const postsStore = usePostsStore()
+
+const postFilter = ref('All')
+
+const setPostFilter = () => {
+  postFilter.value = postFilter.value === 'All' ? 'Saved' : 'All'
+}
 </script>
 
 <template>
   <div class="-mx-4 p-6 flex justify-between items-center border-b">
-    <h3 class="text-xl sm:text-3xl">All posts</h3>
-    <button class="bg-blue-500 px-2 py-1 rounded-md text-white">Show saved posts</button>
+    <div class="flex items-center justify-between gap-4">
+      <h3 class="text-xl sm:text-3xl">{{ postFilter === 'All' ? 'All posts' : 'Saved posts' }}</h3>
+      <span class="material-icons animate-spin" v-show="postsStore.loading">cached</span>
+    </div>
+    <button @click="setPostFilter" class="bg-blue-500 px-2 py-1 rounded-md text-white">
+      {{ postFilter === 'All' ? 'Show saved posts' : 'Show all posts' }}
+    </button>
   </div>
   <div class="mt-6">
-    <div v-for="post in postsStore.sorted" :key="post.id" class="mb-6">
-      <MyWrapper>
-        <PostItem :post="post" />
-      </MyWrapper>
+    <div v-if="postFilter === 'All'">
+      <div v-for="post in postsStore.sorted" :key="post.id" class="mb-6">
+        <MyWrapper>
+          <PostItem :post="post" />
+        </MyWrapper>
+      </div>
+    </div>
+
+    <div v-if="postFilter === 'Saved'">
+      <div v-for="post in postsStore.saved" :key="post.id" class="mb-6">
+        <MyWrapper>
+          <PostItem :post="post" />
+        </MyWrapper>
+      </div>
     </div>
   </div>
 </template>
